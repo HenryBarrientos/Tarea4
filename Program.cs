955afe0847f85d1932131ba123b815b8275bd9b7
Henry Barrientos Flores <henryflo1389@gmail.com>
2021-03-19 17:18:37 -0600

    tarea4

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Tarea61
+{
+    class Program
+    {
+        
+            public static void dibuja(int Cantidad)
+            {
+                int alto = 56;
+                int largo = 80;
+                int lado2= 5;
+                int lado1= 5;
+                int F, C, A;
+             
+                for (A = 1; A <= Cantidad; A++)
+                {
+                    for (F = lado1; F <= alto; F++)
+                    {
+                        Console.SetCursorPosition(F, alto); Console.Write("l"); Console.SetCursorPosition(F, lado2); Console.Write("*");
+                    }
+                    for (C = lado2; C <= largo; C++)
+                    {
+                        Console.SetCursorPosition(lado1, C); Console.Write("#");
+                        Console.SetCursorPosition(alto, C); Console.Write("@");
+                    }
+                    lado2 = lado2 + 1;
+                    alto = alto  - 1;
+                    lado1= lado1+ 1;
+                    largo  = largo - 1;
+                }
+
+
+            }
+
+
+            static void Main(string[] args)
+            {
+                for (int tam = 1; tam < 9; tam++)
+                {
+                    dibuja(tam);
+                    Thread.Sleep(1000);
+                }
+                Console.SetCursorPosition(25, 24);
+                Console.Write("Pulse una tecla ");
+            }
+        }
+    }
---
+++ ej6-2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace tarea62
+{
+    class Program
+    {
+        static string Mid(string parametro, int startIndex, int length)
+        {
+            string result = parametro.Substring(startIndex, length); return result;
+        }
+        static void Main(string[] args)
+        {
+            string nombre;
+            string l;
+            int K = 0;
+            int orden = 0;
+            int letra = 0;
+            int final = 0;
+            Console.Write(" Ingrese su nombre ");
+            nombre = Console.ReadLine();
+            Console.SetCursorPosition(1, 12);
+            Console.Write(nombre);
+            letra = nombre.Length;
+            final = 70;
+            nombre = nombre.ToUpper();
+            for (orden = letra; orden >= 1; orden --)
+            {
+                l = Mid(nombre, orden- 1, 1);
+                for (K = letra; K <= final; K++)
+                {
+                    Console.SetCursorPosition(K, 12);
+                    Console.Write(" " + l);
+                    Thread.Sleep(50);
+                }
+                final = final - 1;
+                letra = letra - 1;
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
